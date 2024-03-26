733c96a3d43b6664b719c5c9d8dd57dcce4ec606
x <guxi@trytryc.com>
2024-03-26 19:41:24 +0800

    test copy

--- src/hooks/useTonConnect.ts
+++ src/hooks/useTonConnect.ts
@@ -14,16 +14,18 @@ export function useTonConnect(): {
   return {
     sender: {
       send: async (args: SenderArguments) => {
-        tonConnectUI.sendTransaction({
-          messages: [
-            {
-              address: args.to.toString(),
-              amount: args.value.toString(),
-              payload: args.body?.toBoc().toString("base64"),
-            },
-          ],
-          validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
-        });
+        console.log('tonConnectUI')
+        console.log(tonConnectUI.getWallets())
+        // tonConnectUI.sendTransaction({
+        //   messages: [
+        //     {
+        //       address: args.to.toString(),
+        //       amount: args.value.toString(),
+        //       payload: args.body?.toBoc().toString("base64"),
+        //     },
+        //   ],
+        //   validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
+        // });
       },
     },
     connected: !!wallet?.account.address,
