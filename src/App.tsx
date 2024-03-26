1980ad2ce72ca5463363049d29180d6cf629d97d
x <guxi@trytryc.com>
2024-03-26 19:19:45 +0800

    test copy

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,9 @@ function App() {
                 : "N/A"}
             </Button>
           </FlexBoxRow>
-          <Counter />
-          <TransferTon />
-          <Jetton />
+          {/*<Counter />*/}
+          {/*<TransferTon />*/}
+          {/*<Jetton />*/}
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
